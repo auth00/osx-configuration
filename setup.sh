296ec98b4608794d4e65b69ef4ee96f060bb2d01
Thomas Augustsson <thomas@toad.lan>
2016-06-16 22:28:59 +0200

    Refactored and added ssh config

--- setup.sh
+++ setup.sh
@@ -6,14 +6,33 @@ TIMESTAMP=`date +%s`
 # Path to where configuration files are cloned
 CONFIGURATION_PATH="`dirname $0`"
 
+HOME_REALPATH=`realpath ~`
+
 echo "Setting up home dot files as symlinks..."
-for FILE_PATH in `ls -1 $CONFIGURATION_PATH/home_dot_files_as_symlinks/_*`; do
-  FILE_NAME=`basename $FILE_PATH`
-  FILE_NAME_WITHOUT_=${FILE_NAME:1}
-  FILE_AT_HOME=`realpath -s ~/.$FILE_NAME_WITHOUT_`
+for FILE in `find $CONFIGURATION_PATH/symlinks -type file`; do
+  FILE_NAME=`basename $FILE`
+  if [ "${FILE_NAME:0:1}" = "_" ]; then
+    FILE_NAME_TO_USE=".${FILE_NAME:1}"
+  else
+    FILE_NAME_TO_USE="$FILE_NAME"
+  fi
+  CONFIGURATION_PATH_TO_FILES="$CONFIGURATION_PATH/symlinks"
+  CONFIGURATION_PATH_LENGTH=${#CONFIGURATION_PATH_TO_FILES}
+  FILE_PATH="`dirname $FILE`/"
+  FILE_PATH=${FILE_PATH:$CONFIGURATION_PATH_LENGTH}
+  if [ "${FILE_PATH:0:1}" = "_" ]; then
+    FILE_PATH_TO_USE=".${FILE_PATH:1}"
+  else
+    FILE_PATH_TO_USE="$FILE_PATH"
+  fi
+  DIR_REPLACE_SOURCE="/_"
+  DIR_REPLACE_TARGET="/."
+  FILE_PATH_TO_USE="${FILE_PATH_TO_USE/$DIR_REPLACE_SOURCE/$DIR_REPLACE_TARGET}"
+  FILE_AT_HOME_FILE="~""$FILE_PATH_TO_USE$FILE_NAME_TO_USE"
+  FILE_AT_HOME=`realpath -s $HOME_REALPATH$FILE_PATH_TO_USE$FILE_NAME_TO_USE`
   FILE_AT_HOME_BACKUP="$FILE_AT_HOME.$TIMESTAMP"
-  FILE_AT_CONFIGURATION_PATH=`realpath $FILE_PATH`
-  if [ -e $FILE_AT_HOME ]; then
+  FILE_AT_CONFIGURATION_PATH=`realpath $FILE`
+  if [ -e $FILE_AT_HOME ] || [ -h $FILE_AT_HOME ]; then
     if [ -h $FILE_AT_HOME ]; then
       if [ "$(readlink $FILE_AT_HOME)" = "$FILE_AT_CONFIGURATION_PATH" ]; then
         echo "* Skipping $FILE_AT_HOME, already set"
